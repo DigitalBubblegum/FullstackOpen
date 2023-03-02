f85dfe380cbfca552ad0c56594590bf31ce6da37
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-03-02 19:00:36 +0530

    - followed example to re-render components

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -10,17 +10,19 @@ const Hello = ({ name, age }) => {
   )
 }
 
-const App = () => {
+const App = (props) => {
+  const {counter} = props
   const name = 'Peter'
   const age = 24
 
 
   return (
+    <><div>{counter}</div>
     <div>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
-    </div>
+    </div></>
   )
 }
 
--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,5 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
+let counter = 1 
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const refresh = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter={counter} />
+  )
+}
+
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
