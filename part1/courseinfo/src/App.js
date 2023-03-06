be15696d2a9ba9d318f7746e5d42e1548ccfca33
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-03-06 16:32:55 +0530

    -followed example to refactor complex state code

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,37 +1,57 @@
 import { useState } from "react"
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [comp, setComp] = useState('You have not yet clicked to compare')
-  const compareValues = () => {
-    if (left>right) {
-      console.log('left is greater')
-      return(<>Left is greater than right</>)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+  const[comp, setComp] = useState('not compared yet')
+  const handleLeftClick = () => {
+    const newClicks = { 
+      left: clicks.left + 1, 
+      right: clicks.right 
     }
-    else if (right > left) {
+    setClicks(newClicks)
+  }
+
+  const handleRightClick = () => {
+    const newClicks = { 
+      left: clicks.left, 
+      right: clicks.right + 1 
+    }
+    setClicks(newClicks)
+  }
+
+  const handleCompClick = () => {
+    let newComp = ''
+    if (clicks.left>clicks.right) {
+      console.log('left is greater')
+      newComp = <>Left is greater than right</>
+    }
+    else if (clicks.right > clicks.left) {
       console.log('right is greater')
-      return(<>Right is greater than left</>)
+      newComp = <>Right is greater than left</>
     } 
     else {
       console.log('they are equal')
-      return(<>They are equal</>)
+      newComp = <>They are equal</>
     }
+    setComp(newComp)
   }
+
   return (
     <>
-      <div>
-        {left}
-        <button onClick={() => setLeft(left + 1)}>
-        left
-        </button>
-        <button onClick={() => setRight(right + 1)}>
-        right
-        </button>
-      {right}
+    <div>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
-    <button onClick={() => setComp(compareValues)}>Click to compare</button>
-    <p><>{comp}</></p>
+    <button onClick={handleCompClick}>compare the values</button>
+    <p>
+      <>
+      {comp}
+      </>
+    </p>
     </>
     
   )
