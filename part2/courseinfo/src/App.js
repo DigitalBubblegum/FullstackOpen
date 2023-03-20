372e047204d2142362941b4b0348d5ba00b60e50
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-03-20 12:57:34 +0530

    -followed example refactoring modules in part 2

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,12 +1,12 @@
-const App = (props) => {
-  const { notes } = props
+import Note from './components/Note'
+const App = ({notes}) => {
   const notes_imp = notes.map(note => note.important)
   console.log(notes_imp)
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <li key = {note.id}>{note.content}</li>)}
+        {notes.map(note => <Note key = {note.id} note={note} />)}
       </ul>
     </div>
   )
