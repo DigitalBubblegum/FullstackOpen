2db5fe1b3cc5d8963af3cbb6d9cd50e6176914f4
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-03-20 12:40:08 +0530

    -followed along with mapping example of part 2

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,13 +1,12 @@
 const App = (props) => {
   const { notes } = props
-
+  const notes_imp = notes.map(note => note.important)
+  console.log(notes_imp)
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notes.map(note => <li key = {note.id}>{note.content}</li>)}
       </ul>
     </div>
   )
