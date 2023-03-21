f5a5a14ce121bc26c1565a7f2d9f50e4da45f5a5
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-03-21 20:14:55 +0530

    -made chanhes to part2/courseinfo

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,7 +1,9 @@
 import Note from './components/Note'
 const App = ({notes}) => {
   const notes_imp = notes.map(note => note.important)
+  const notes_id  = notes.map(note => note.id)
   console.log(notes_imp)
+  console.log(notes_id);
   return (
     <div>
       <h1>Notes</h1>
