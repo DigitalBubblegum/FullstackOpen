edc8421713359eaac1bd36badabdf904a7a17609
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-03-27 19:56:41 +0530

    -followed example for Controlled components

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -3,9 +3,22 @@ import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+  //functions
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value)
+  }
   const addNote = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target);
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('Add a new note')
   }
   return (
     <div>
@@ -16,7 +29,7 @@ const App = (props) => {
           )}
       </ul>
       <form onSubmit={addNote}>
-        <input />
+        <input value={newNote} onChange = {handleNoteChange}/>
         <button type = "submit">save</button>
       </form>
     </div>
