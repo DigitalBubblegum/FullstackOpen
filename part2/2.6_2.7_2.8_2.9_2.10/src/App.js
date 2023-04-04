3e00df730d2574a9640f6140e898035e8bca75ef
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-04-04 08:53:00 +0530

    -completed exercise 2.8

--- part2/2.6_2.7_2.8_2.9_2.10/src/App.js
+++ part2/2.6_2.7_2.8_2.9_2.10/src/App.js
@@ -2,17 +2,17 @@ import { useState } from 'react'
 
 const Person = ({persons}) => {
   console.log(persons.map((person)=>person.name))
-  return(<ul>{persons.map((person)=><li key={person.id}>{person.name}</li>)}
+  return(<ul>{persons.map((person)=><li key={person.id}>{person.name} - {person.phone}</li>)}
   </ul>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas' }, 
+    { id: 1, name: 'Arto Hellas', phone: '111111' }, 
   ]) 
   const [newName, setNewName] = useState('')
-
+  const [newPhone, setNewPhone] = useState('')
   //functions
 
   const addPerson = (event) =>{
@@ -23,12 +23,15 @@ const App = () => {
       const numberObject = {
         id: persons.length + 1,
         name: newName,
+        phone: newPhone,
       }
       setPersons(persons.concat(numberObject))
       setNewName('')
+      setNewPhone('')
     } else {
       alert (`${newName} is already added to phonebook`)
       setNewName('')
+      setNewPhone('')
     }
   }
 
@@ -41,6 +44,10 @@ const App = () => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
+  const handlePhoneChange = (event) =>{
+    console.log(event.target.value)
+    setNewPhone(event.target.value)
+  }
   
   return (
     <div>
@@ -48,6 +55,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange = {handleNameChange}/>
+        </div>
+        <div>
+          number: <input value = {newPhone} onChange={handlePhoneChange}/>
         </div>
         <div>
           <button type="submit">add</button>
