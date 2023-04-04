7a473762eaedc8f62794a8c75756549b3b7e9558
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-04-04 08:30:54 +0530

    -finished exercise 2.6

--- part2/2.6_2.7_2.8_2.9_2.10/src/App.js
+++ part2/2.6_2.7_2.8_2.9_2.10/src/App.js
@@ -1,27 +1,47 @@
 import { useState } from 'react'
 
+const Person = ({persons}) => {
+  console.log(persons.map((person)=>person.name))
+  return(<ul>{persons.map((person)=><li key={person.id}>{person.name}</li>)}
+  </ul>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { id: 1, name: 'Arto Hellas' }, 
   ]) 
   const [newName, setNewName] = useState('')
+
   //functions
-  const submitHandler = (event) =>{
+  const addPerson = (event) =>{
     event.preventDefault()
+    console.log('button clicked');
+    const numberObject = {
+      id: persons.length + 1,
+      name: newName,
+    }
+    setPersons(persons.concat(numberObject))
+    setNewName('')
   }
+  const handleNameChange = (event) =>{
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={submitHandler}>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange = {handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <Person persons = {persons}/>
     </div>
   )
 }
--- part2/2.6_2.7_2.8_2.9_2.10/src/index.js
+++ part2/2.6_2.7_2.8_2.9_2.10/src/index.js
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.render(
-  <App />, 
-  document.getElementById('root')
-)
+const root = document.getElementById('root')
+const app = <App />
+
+ReactDOM.createRoot(root).render(app)
