d251ef5095d5a254ce7f3e25ce0a674aa94b2bf0
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-04-07 21:43:39 +0530

    -following example for promises

--- part2/notes/src/index.js
+++ part2/notes/src/index.js
@@ -1,7 +1,10 @@
 import ReactDOM from "react-dom/client"
-
+import axios from "axios";
 import App from "./App"
-
+const promise = axios.get("http://localhost:3001/notes")
+console.log(promise);
+const promise2 = axios.get("http://localhost:3001/foobar")
+console.log(promise2);
 const notes = [
   {
     id: 1,
