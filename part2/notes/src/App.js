3a49829852e4266f87b5588b69a652e0def15712
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-04-15 14:04:33 +0530

    -followed along example for adding css to notes

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,12 +1,12 @@
 import {useState,useEffect} from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
-
+import Notification from "./components/Notification";
 const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
-
+  const [errorMessage, setErrorMessage] = useState(null)
   //effects
     useEffect(() => {
     console.log('effect')
@@ -26,7 +26,12 @@ const toggleImportanceOf = id => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
   })
   .catch(error => {
-    alert(`the note '${note.content}' was already removed from the server` )
+    setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
     setNotes(notes.filter(n => n.id !== id))
   })
 }
@@ -55,6 +60,7 @@ const toggleImportanceOf = id => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
