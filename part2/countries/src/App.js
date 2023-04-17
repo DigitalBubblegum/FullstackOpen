48d0012eeb86a403793e7e2b5a162170f4e185e9
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-04-17 08:29:03 +0530

    -created code for part2/countries

---
+++ part2/countries/src/App.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='Heading1'>
+        Countries
+      </h1>
+    </div>
+  );
+}
+
+export default App;
