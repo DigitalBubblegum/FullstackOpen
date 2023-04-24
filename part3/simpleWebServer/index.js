17acd9dcaa215eba5a4af23dfc854f98d4a5a951
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-04-24 08:50:46 +0530

    Revert "-made changes to simplewebserver backend"
    This reverts commit 79ad4d00eae74aab5daf6ee784d4e20e675852d9.

--- part3/simpleWebServer/index.js
+++ part3/simpleWebServer/index.js
@@ -3,7 +3,6 @@ const app = express()
 const cors = require("cors");
 app.use(cors());
 app.use(express.json())
-app.use(express.static("build"));
 let notes = [
   {
     id: 1,
@@ -70,7 +69,6 @@ app.delete('/api/notes/:id',(request,response)=>{
   response.status(204).end()
   console.log('deleted');
 })
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
