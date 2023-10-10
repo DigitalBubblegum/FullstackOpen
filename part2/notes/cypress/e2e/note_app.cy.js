58228684fa122e09a4e46ec6bc435e5a5108d0c0
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-10-10 13:00:12 +0530

    -minor refactor

--- part2/notes/cypress/e2e/note_app.cy.js
+++ part2/notes/cypress/e2e/note_app.cy.js
@@ -1,23 +1,22 @@
 describe('Note app', function() {
   beforeEach(function () {
     cy.visit('http://localhost:3001')
+    cy.contains('Login').click()
+    cy.get('#username').type('testUser')
+    cy.get('#password').type('123456789')
+    cy.get('#login-button').click()
   })
   it('front page can be opened', function() {
     cy.contains('Notes')
     cy.contains('this is one note')
   })
   it('user can login',function () {
-    cy.contains('Login').click()
-    cy.get('#username').type('testUser')
-    cy.get('#password').type('123456789')
-    cy.get('#login-button').click()
     cy.contains('test logged in')
   })
+  it('user can create a new note after a successul login',function () {
+    cy.contains('new note').click()
+  })
   it('user can logout',function () {
-    cy.contains('Login').click()
-    cy.get('#username').type('testUser')
-    cy.get('#password').type('123456789')
-    cy.get('#login-button').click()
     cy.contains('logout').click()
   })
 })
