33f048ee8ab69a940150047689c9cc0ec6ebfb7d
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-10-10 18:30:07 +0530

    -test for login fail when wrong credentials

--- part2/notes/cypress/e2e/note_app.cy.js
+++ part2/notes/cypress/e2e/note_app.cy.js
@@ -35,5 +35,11 @@ describe('Note app', function() {
     cy.get('#username').type('mluukkai')
     cy.get('#password').type('wrong')
     cy.get('#login-button').click()
+    cy.get('.error')
+      .should('contain', 'Wrong credentials')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'border-style', 'solid')
+
+    cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
   })
 })
