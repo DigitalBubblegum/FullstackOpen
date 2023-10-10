15b89096f64275dd9dc16f58c0cfe84c9d476411
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-10-10 17:40:55 +0530

    -login test

--- part2/notes/cypress/e2e/note_app.cy.js
+++ part2/notes/cypress/e2e/note_app.cy.js
@@ -1,20 +1,26 @@
 describe('Note app', function() {
   beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3001')
     cy.contains('Login').click()
-    cy.get('#username').type('testUser')
-    cy.get('#password').type('123456789')
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('salainen')
     cy.get('#login-button').click()
   })
   it('front page can be opened', function() {
     cy.contains('Notes')
-    cy.contains('this is one note')
   })
   it('user can login',function () {
-    cy.contains('test logged in')
+    cy.contains('Matti Luukkainen logged in')
   })
   it('user can create a new note after a successul login',function () {
-    cy.contains('test logged in')
+    cy.contains('Matti Luukkainen logged in')
     cy.contains('new note').click()
     cy.get('#submit-form').type('a note created by cypress')
     cy.contains('save').click()
@@ -22,5 +28,12 @@ describe('Note app', function() {
   })
   it('user can logout',function () {
     cy.contains('logout').click()
+  })
+  it('displays wrong credential if credentials are wrong', function() {
+    cy.contains('logout').click()
+    cy.contains('Login').click()
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('wrong')
+    cy.get('#login-button').click()
   })
 })
