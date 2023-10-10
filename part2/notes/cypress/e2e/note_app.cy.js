865cb149ca6657c69316de8d95d1c13cd18e4a3e
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-10-10 11:44:55 +0530

    -small prelim tests

--- part2/notes/cypress/e2e/note_app.cy.js
+++ part2/notes/cypress/e2e/note_app.cy.js
@@ -1,7 +1,23 @@
 describe('Note app', function() {
-  it('front page can be opened', function() {
+  beforeEach(function () {
     cy.visit('http://localhost:3001')
+  })
+  it('front page can be opened', function() {
     cy.contains('Notes')
     cy.contains('this is one note')
+  })
+  it('login form can be opened',function () {
+    cy.contains('login').click()
+  })
+  it('user can login',function () {
+    cy.get('input:first').type('testUser')
+    cy.get('input:last').type('123456789')
+    cy.contains('login').click()
+  })
+  it('user can logout',function () {
+    cy.get('input:first').type('testUser')
+    cy.get('input:last').type('123456789')
+    cy.contains('login').click()
+    cy.contains('logout').click()
   })
 })
